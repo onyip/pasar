5d8bf4dbb9e73e6b3538e2ecafb0e0c483d0b82e
onyipgtg <onyipgtg@gmail.com>
2021-01-07 00:03:40 +0700

    update welcome page

--- lib/ui/auth/welcome_page.dart
+++ lib/ui/auth/welcome_page.dart
@@ -8,47 +8,106 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(margin: EdgeInsets.only(top: 20),
-              child: Text("Selamat Datang Di", style: TextStyle(fontSize: 20),),
-            ),
-
-            Container(
-              child: Text(
-                "SIMBOK BLONJO",
-                style: TextStyle(
-                  fontSize: 30,
+        child: Container(
+          color: Colors.white70,
+          child: Column(
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(
+                  child: Image(
+                    image: NetworkImage(
+                        'https://as1.ftcdn.net/jpg/02/44/61/66/500_F_244616624_UcJucFEN467oUhqhHcBEhBYj7SKV2sFN.jpg'),
+                  ),
                 ),
               ),
-            ),
-            
-            Expanded(child: Icon(Icons.add_shopping_cart)),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: RaisedButton(
-                      onPressed: () {
-                        BlocProvider.of<AuthBloc>(context)
-                            .add(AuthToRegister());
-                      },
-                      child: Text('Daftar'),
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.only(top: 20),
+                      child: Text(
+                        "Selamat Datang Di",
+                        style: TextStyle(fontSize: 18),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: RaisedButton(
-                      onPressed: () {
-                        BlocProvider.of<AuthBloc>(context).add(AuthToLogin());
-                      },
-                      child: Text('Masuk'),
+                    Container(
+                      child: Text(
+                        "SIMBOK BLONJO",
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
                     ),
-                  )
-                ],
+                    Container(
+                      child: Text(
+                        "Dinas Perindustrian dan Perdagangan",
+                        style: TextStyle(fontSize: 11),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Kabupaten Kebumen",
+                        style: TextStyle(fontSize: 11),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            )
-          ],
+              Container(
+                margin: EdgeInsets.only(
+                  left: 10,
+                  right: 10,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      child: RaisedButton(
+                        onPressed: () {
+                          BlocProvider.of<AuthBloc>(context)
+                              .add(AuthToRegister());
+                        },
+                        padding: EdgeInsets.only(
+                          left: 50,
+                          right: 50,
+                        ),
+                        color: Colors.blue[900],
+                        child: Text(
+                          'Daftar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: OutlineButton(
+                        onPressed: () {
+                          BlocProvider.of<AuthBloc>(context).add(AuthToLogin());
+                        },
+                        borderSide: BorderSide(
+                          width: 1,
+                          style: BorderStyle.solid,
+                        ),
+                        padding: EdgeInsets.only(
+                          left: 50,
+                          right: 50,
+                        ),
+                        child: Text('Masuk'),
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
